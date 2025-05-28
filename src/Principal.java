5c93ac680da1ff4f8395b4196545354f3642d264
ovzm <victorzawacki9@gmail.com>
2025-05-28 00:02:23 -0300

    Commit struct

---
+++ src/Principal.java
@@ -0,0 +1,10 @@
+public class Principal {
+    public static void main(String[] args) {
+       Filme filme = new Filme();
+
+       filme.exibeFichaTecnica();
+       filme.avalia(8);
+       filme.avalia(5);
+       filme.avalia(4);
+    }
+}
