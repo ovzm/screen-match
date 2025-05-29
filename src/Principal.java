f4c32fb67fb98b7d5093f1a5133b3a4c4b8ca3c9
ovzm <victorzawacki9@gmail.com>
2025-05-29 01:03:28 -0300

    Commit set e get

--- src/Principal.java
+++ src/Principal.java
@@ -1,10 +1,18 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
        Filme filme = new Filme();
+       filme.setNome("Capitão America");
+       filme.setDuracaoEmMinutos(180);
+       filme.setAnoDeLancamento(2020);
+
 
        filme.exibeFichaTecnica();
        filme.avalia(8);
        filme.avalia(5);
        filme.avalia(4);
+
+        System.out.println("Total de avaliações: " + filme.getTotalAvaliacao());
     }
 }
